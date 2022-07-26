4298c5a4dd1a4014128b06a4a84d843024525e22
LuizGustavo <202211020354@faculdadenovaroma.com.br>
2022-07-25 21:37:00 -0300

    Expressjs e entendo o back com front

--- app/server.js
+++ app/server.js
@@ -1,14 +1,40 @@
-const app = require('./lexjson');
+const express = require("express");
+const getStatus = require('./lexjson');
+const app = express();
+const PORTA = 80;
 
+//Lista de impressoras
 let impressoras = [
     '172.16.140.164'
 ];
 
+app.get('/', (req,res) => {
+    res.sendFile("C:\\Code\\gitcodes\\Insumo-Impressoras\\web\\dashboard.html");
+});
+
+app.post('/cadastro', (req,res) => {
+    res.send('Tela de Cadastro');
+})
+
+
+
+
+app.listen(PORTA, () => {
+    console.log(`Servidor iniciado na ports ${PORTA}`);
+})
+
+
+
+//Implementas isso aqui dentro das rotas requisitadas.
+/*
+
 let statusImpressoras = [];
 
 impressoras.forEach(x => {
-    app.pegaStatus(x, function(resultado){
-        statusImpressoras.push(resultado);
+    let objResposta = app.pegaStatus(x, function(resultado){
+        return resultado;
    });
 });
+*/
+
 
