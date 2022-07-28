2ea615b98199518f73e475886089ff3a8ad8c427
My balls <something@gmail.com>
2022-07-28 13:01:54 -0300

    Esta retornando os valores, mas esses vem na ordem de resposta Vou precisar organizar o array de resposta para bater com os cadastros.

--- app/server.js
+++ app/server.js
@@ -3,6 +3,8 @@ const app = express();
 const PORTA = 8080;
 const getStatus = require('./impressoras');
 
+app.use(express.static('D:\\Luiz\\Codes\\Insumo Impressoras\\web'))
+
 app.get('/', (req,res) => {
     res.sendFile("D:\\Luiz\\Codes\\Insumo Impressoras\\web\\dashboard.html");
 });
@@ -10,10 +12,8 @@ app.get('/', (req,res) => {
 app.post('/cadastro', (req,res) => {
     getStatus.resposta(function(resposta){
         res.send(resposta);
-    })
-    
+    })  
 })
-
 
 
 app.listen(PORTA, () => {
