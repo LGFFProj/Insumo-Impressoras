981267eb5a329aa578a92a1a687fa100015be6c8
My balls <something@gmail.com>
2022-08-10 16:39:33 -0300

    Fazendo uma barra de busca pra as impressoras

---
+++ views/barra.js
@@ -0,0 +1,7 @@
+function barra()
+let input, filtro, valor, ul, lista;
+
+input = document.getElementById('input');
+filtro = input.value.toUpperCase();
+ul = document.getElementById('UL');
+lista = ul.getElementsByTagName
--- views/index.js
+++ views/index.js
@@ -2,21 +2,29 @@
 document.getElementById('dashboard').addEventListener('click', () => {
     document.getElementById('container_cadastro').style.display = 'none';
     document.getElementById('container_relatorio').style.display = 'none';
-    document.getElementById('container_dashboard').style.display = 'block';
-
+    document.getElementById('container_dashboard').style.display = 'flex';
+    document.getElementById('dashboard').style.backgroundColor = '#c2c2c2';
+    document.getElementById('cadastro').style.backgroundColor = '#2b63cb83';
+    document.getElementById('relatorio').style.backgroundColor = '#2b63cb83';
 })
 
 //Evento para quando apertar o botão "CADASTRO"
 document.getElementById('cadastro').addEventListener('click', () => {
     document.getElementById('container_dashboard').style.display = 'none';
     document.getElementById('container_relatorio').style.display = 'none';
-    document.getElementById('container_cadastro').style.display = 'block';
+    document.getElementById('container_cadastro').style.display = 'flex';
+    document.getElementById('cadastro').style.backgroundColor = '#c2c2c2'
+    document.getElementById('dashboard').style.backgroundColor = '#2b63cb83'
+    document.getElementById('relatorio').style.backgroundColor = '#2b63cb83';
 })
 
 //Evento para quando apertar o botão "RELATÓRIO"
 document.getElementById('relatorio').addEventListener('click', () => {
     document.getElementById('container_dashboard').style.display = 'none';
     document.getElementById('container_cadastro').style.display = 'none';
-    document.getElementById('container_relatorio').style.display = 'block';
+    document.getElementById('container_relatorio').style.display = 'flex';
+    document.getElementById('relatorio').style.backgroundColor = '#c2c2c2';
+    document.getElementById('dashboard').style.backgroundColor = '#2b63cb83';
+    document.getElementById('cadastro').style.backgroundColor = '#2b63cb83';
 
 })
